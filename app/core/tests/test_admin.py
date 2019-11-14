70561713a575470d278c8115f24d138997a8f666
Ilya Bass <ia1717@gmail.com>
2019-11-14 10:04:58 -0500

    more users and tests

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,30 @@
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@admin.com',
+            password = 'whatever12'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'user@user.com',
+            password = 'whatever13',
+            name = 'Test Full Name'
+        )
+
+    def test_users_listed(self):
+        url = reverse('admin:core_user_changelist')
+        resp = self.client.get(url)
+        self.assertContains(resp, self.user.name)
+        self.assertContains(resp, self.user.email)
+
+    def test_user_change_page(self):
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        # admin/core/user/1
+        res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
