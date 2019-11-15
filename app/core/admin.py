53c050845f01000708cca3e82bd82db061d7b222
Ilya Bass <ia1717@gmail.com>
2019-11-14 19:40:17 -0500

    wait for db

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,22 +3,28 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core import models
 from django.utils.translation import gettext as _
 
+
 class UserAdmin(BaseUserAdmin):
     ordering = ['id']
     list_display = ['email', 'name']
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
-        (_('Personal Info'), {'fields' : ('name',)}),
+        (_('Personal Info'), {'fields': ('name',)}),
         (
             _('Permissions'),
             {
-                'fields' : ('is_active', 'is_staff', 'is_superuser')
+                'fields': ('is_active', 'is_staff', 'is_superuser')
             }
         ),
-        (_('Important dates'), {'fields': ('last_login',) })
+        (_('Important dates'), {'fields': ('last_login',)})
     )
 
-    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide'),
+            'fields': ('email', 'password1', 'password2')
+        }),
+    )
+
 
 admin.site.register(models.User, UserAdmin)
-
--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -2,19 +2,20 @@ from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 
+
 class AdminSiteTests(TestCase):
 
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email = 'admin@admin.com',
-            password = 'whatever12'
+            email='admin@admin.com',
+            password='whatever12'
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email = 'user@user.com',
-            password = 'whatever13',
-            name = 'Test Full Name'
+            email='user@user.com',
+            password='whatever13',
+            name='Test Full Name'
         )
 
     def test_users_listed(self):
@@ -27,4 +28,10 @@ class AdminSiteTests(TestCase):
         url = reverse('admin:core_user_change', args=[self.user.id])
         # admin/core/user/1
         res = self.client.get(url)
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
         self.assertEqual(res.status_code, 200)
